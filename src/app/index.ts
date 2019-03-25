6d422d80b0925991cbb7e17e0371fd1913a5211d
yaoandy107 <yaoandy107@gmail.com>
2019-03-25 22:48:32 +0800

    Add middlewares

--- src/app/index.ts
+++ src/app/index.ts
@@ -1,11 +1,42 @@
 import Koa from 'koa'
 import bodyparser from 'koa-bodyparser'
+import compress from 'koa-compress'
+import helmet from 'koa-helmet'
+import json from 'koa-json'
+import logger from 'koa-logger'
+import onerror from 'koa-onerror'
+import protect from 'koa-protect'
+import responseTime from 'koa-response-time'
 import router from './routers/'
 
 const app: Koa<{}, any> = new Koa()
 
+onerror(app)
+
+// X-Response-Time middleware
+app.use(responseTime())
+// JSON pretty-printed response middleware
+app.use(json())
+// Development style logger middleware
+app.use(logger())
+// SQL injection  protection middleware
+app.use(protect.koa.sqlInjection({
+  body: true,
+  loggerFunction: console.error,
+}))
+// XSS protection middleware
+app.use(protect.koa.xss({
+  body: true,
+  loggerFunction: console.error,
+}))
+// Header security middleware
+app.use(helmet())
+// Compress packet middleware
+app.use(compress())
+// Formdata parser middleware
 app.use(bodyparser())
 
+// Add all the api routers
 app.use(router.routes()).use(router.allowedMethods())
 
 export default app
